02fc1327951709885962053c823612cbc00113b7
David <david2009live@hotmail.fr>
2019-11-29 14:20:09 +0100

    fix #2 Tests sur QuestionAChoixMultiple

---
+++ src/test/java/miagem1/QuestionAChoixMultipleTest.java
@@ -0,0 +1,55 @@
+package miagem1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class QuestionAChoixMultipleTest {
+
+    private QuestionAChoixMultiple question;
+
+    //Permet de donner le Given commun à toutes les méthodes
+
+    @Before
+    public void setUp() throws Exception {
+        //Given : Une instance QuestionAChoixMultiple
+        ArrayList<Integer> indices = new ArrayList<Integer>();
+        indices.add(1);
+        indices.add(3);
+        question = new QuestionAChoixMultiple("Un énoncé", indices);
+    }
+
+    @Test
+    public void getEnonce() {
+        //When On demande son énoncé à la question
+        String repEnonce = question.getEnonce();
+
+        //Then L'énoncé retourné est l'énoncé fourni à la construction de l'objet
+        assertEquals("Un énoncé", repEnonce);
+    }
+
+    @Test
+    public void getScoreForIndiceBonneReponse() {
+        //When quand l'étudiant fourni l'indice d'une bonne réponse
+        int indiceEtudiant = 1;
+        //et que on demande le score de l'indice à la question
+        float score = question.getScoreForIndice(indiceEtudiant);
+
+        //Then Le score obtenu est 50
+        assertEquals(50, score, 0.01f);
+    }
+
+    @Test
+    public void getScoreForIndiceEchec() {
+        //When quand l'étudiant fourni l'indice d'une mauvaise réponse
+        int indiceEtudiant = 0;
+        //et que on demande le score de l'indice à la question
+        float score = question.getScoreForIndice(indiceEtudiant);
+
+        //Then Le score obtenu est 0
+        assertEquals(0f, score, 0.01f);
+    }
+}
